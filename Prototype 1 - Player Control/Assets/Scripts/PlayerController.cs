cc772d6addc0e4085625e6fd222329bd733afa8e
a-jennings <andrew_jennings@ymail.com>
2020-04-22 15:25:29 +0100

    added player movement

--- Prototype 1 - Player Control/Assets/Scripts/PlayerController.cs
+++ Prototype 1 - Player Control/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //Player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        //Vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
+        //Vehicle turn
+        transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput * turnSpeed);
 
     }
 }
